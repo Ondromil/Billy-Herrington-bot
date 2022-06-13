228a548fca4731be091d71e3bfb6b21b9ab57353
Ondřej <ondrahlavac10@gmail.com>
2022-06-13 22:01:40 +0200

    Added more commands

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 from MaxEmbeds import EmbedBuilder
 import random
 
-bot = commands.Bot(command_prefix='!')
-
+bot = commands.Bot(command_prefix='.')
 bot.remove_command('help')
+
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="Sex with Hitler")
+    activity = discord.Game(name="Gachi |.help")
     await bot.change_presence(activity=activity)
     print("Logged as {0.user}".format(bot))
 
@@ -20,14 +20,18 @@ async def on_message(message):
         return
     if message.content.startswith("xd"):
         await message.channel.send("xd")
+    if message.content.startswith("wilson"):
+        await message.channel.send("wilson je absolutní gigachad")
+    if message.content.startswith("fuck you"):
+        await message.channel.send("ah fuck you leatherman")
 
 @bot.event
 async def on_member_remove(member):
-     await message.channel.send(f"{member} leavnul muže :cry:")
+     print(f"{member} leavnul muže :cry:")
 
 @bot.command()
 async def help(ctx):
-    embed = EmbedBuilder(title="Help", description="**!billy** - Start Gachi Muchi rave").build()
+    embed = EmbedBuilder(title="Help", description="**.billy** - Start Gachi Muchi rave").build()
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -36,7 +40,13 @@ async def billy(ctx):
     for i in range(0, 50):
          await ctx.send(words[random.randint(0, 5)])
 
+@bot.command()
+async def enter(ctx):
+    await ctx.send("Hey buddy I think you got the wrong door, the leather club is two blocks down")
+
 with open('Discord-bot\.env') as f:
     Token = f.readline()
+
+cock = "c"
 
 bot.run(Token)
